981a1979113a3016f47a99219c93c85828614af1
Stoyan Chalakov <stch006@hotmail.com>
2024-11-03 21:49:18 +0100

    error handling

--- CreateOverridesInBulk.ps1
+++ CreateOverridesInBulk.ps1
@@ -36,7 +36,7 @@ try {
 
  
 
-#Create a hashtable of all the SCOM classes for faster retreival based on Class ID
+#Create a hashtable of all the SCOM classes for faster retreival based on Class ID. This part has been stoledn from Kevin Holman's blog post: https://blogs.technet.microsoft.com/kevinholman/2016/11/02/using-powershell-to-disable-a-rule-or-monitor-in-scom/
 $ClassHT = @{}
 try {
     $Classes = Get-SCOMClass
@@ -51,24 +51,28 @@ try {
 # Disable all rules by creating overrides in the target Management Pack
 
 foreach ($Rule in $Rules) {
-
-  # Get the targeted class of the rule and other properties
-  [string]$RuleTargetDisplayName = ($ClassHT.($Rule.Target.Id.Guid)).DisplayName
-  $RuleTargetedClass = Get-SCOMClass -DisplayName $RuleTargetDisplayName
-  
-  #Do the actual override
-  Disable-SCOMRule -Class $RuleTargetedClass -Rule $Rule -ManagementPack $TargetMP -Enforce
-
+    try {
+        # Get the targeted class of the rule and other properties
+        [string]$RuleTargetDisplayName = ($ClassHT.($Rule.Target.Id.Guid)).DisplayName
+        $RuleTargetedClass = Get-SCOMClass -DisplayName $RuleTargetDisplayName
+        
+        # Do the actual override
+        Disable-SCOMRule -Class $RuleTargetedClass -Rule $Rule -ManagementPack $TargetMP -Enforce
+    } catch {
+        Write-Error "Failed to disable rule $($Rule.DisplayName): $_"
+    }
 }
 
 # Disable all monitors by creating overrides in the target Management Pack
 foreach ($Monitor in $Monitors) {
+    try {
+        # Get the targeted class of the rule and other properties
+        [string]$MoniTargetDisplayName = ($ClassHT.($Monitor.Target.Id.Guid)).DisplayName
+        $MoniTargetedClass = Get-SCOMClass -DisplayName $MoniTargetDisplayName
 
-  # Get the targeted class of the rule and other properties
-  [string]$MoniTargetDisplayName = ($ClassHT.($Monitor.Target.Id.Guid)).DisplayName
-  $MoniTargetedClass = Get-SCOMClass -DisplayName $MoniTargetDisplayName
-
-  #Do the actual override 
-  Disable-SCOMMonitor -Class $MoniTargetedClass -Monitor $Monitor -ManagementPack $TargetMP -Enforce
-
+        # Do the actual override 
+        Disable-SCOMMonitor -Class $MoniTargetedClass -Monitor $Monitor -ManagementPack $TargetMP -Enforce
+    } catch {
+        Write-Error "Failed to disable monitor $($Monitor.DisplayName): $_"
+    }
 }
